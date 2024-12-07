77f17cf46ba243bcdef84e629d182ad87c39b4bd
Innocent ACOLATSE <inolatse@gmail.com>
2024-12-07 08:39:46 +0000

    updated model with static method

--- app/Models/Enterproduct.php
+++ app/Models/Enterproduct.php
@@ -2,13 +2,17 @@
 
 namespace App\Models;
 
+use Database\Factories\EnterproductFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @method static create(mixed $validated)
+ */
 class Enterproduct extends Model
 {
-    /** @use HasFactory<\Database\Factories\EnterproductFactory> */
+    /** @use HasFactory<EnterproductFactory> */
     use HasFactory;
 
     protected $fillable = [
