53d3b28d6e911dcff2a5af29c227fef9739ad470
Innocent ACOLATSE <inolatse@gmail.com>
2024-12-07 08:39:57 +0000

    Implement all method

--- app/Http/Controllers/EnterproductController.php
+++ app/Http/Controllers/EnterproductController.php
@@ -2,64 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\EnterProductRequest;
 use App\Models\Enterproduct;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class EnterproductController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $enterProducts = Enterproduct::with('product')->get();
+
+        return response()->json($enterProducts);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(EnterProductRequest $request): JsonResponse
     {
-        //
+
+        $enterProduct = Enterproduct::create($request->validated());
+
+        return response()->json($enterProduct, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Enterproduct $enterproduct)
+    public function show(Enterproduct $enterProduct): JsonResponse
     {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Enterproduct $enterproduct)
-    {
-        //
+        $enterProduct ->load('product');
+
+        return response()->json($enterProduct);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Enterproduct $enterproduct)
+    public function update(EnterProductRequest $request, Enterproduct $enterproduct): JsonResponse
     {
-        //
+
+        $enterproduct->update($request->validated());
+
+        return response()->json([
+            'message' => 'Mis a jour effectuer',
+            'data' =>$enterproduct]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Enterproduct $enterproduct)
+    public function destroy(Enterproduct $enterproduct): JsonResponse
     {
-        //
+        $enterproduct->delete();
+
+        return response()->json([
+            'message' => 'Deleted successfully'
+        ]);
     }
 }
